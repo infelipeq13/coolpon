94acc469a4e1b695db7578dc052f706c26d8cb4f
infelipeq13 <70453031+infelipeq13@users.noreply.github.com>
2020-10-10 17:00:16 -0300

    Create Layout component

---
+++ src/components/Layout/index.js
@@ -0,0 +1,14 @@
+import { Image } from "src/components/Image";
+
+import css from "src/components/Layout/Layout.module.css";
+
+export const Layout = ({ children, description, imageName, title }) => {
+	return (
+		<div className={css.container}>
+			<Image className={css.image} name={imageName} />
+			<h1 className={css.title}>{title}</h1>
+			<p className={css.description}>{description}</p>
+			{children}
+		</div>
+	);
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,17 @@
 import { Button } from "src/components/Button";
-import { Image } from "src/components/Image";
 import { Input } from "src/components/Input";
+import { Layout } from "src/components/Layout";
 
 const HomePage = () => {
 	return (
-		<div className="max-w-sm p-4 mx-auto space-y-4 text-center">
-			<Image className="mx-auto" name="paper-plane" />
-			<h1 className="font-bold text-x4">
-				<span className="text-blue-600">Acessar</span> plataforma
-			</h1>
-			<p className="text-x2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent porta maximus lacus.</p>
+		<Layout
+			description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent porta maximus lacus."
+			imageName="paper-plane"
+			title="Acessar plataforma"
+		>
 			<Input label="Endereço de email" type="email" />
-			<Button isExpanded>Enviar link de acesso</Button>
-		</div>
+			<Button isExpanded>Enviar acesso</Button>
+		</Layout>
 	);
 };
 
