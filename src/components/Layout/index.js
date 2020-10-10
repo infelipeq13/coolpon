38c95ea3ca51181cb8ec968db3bafe5063a95d96
infelipeq13 <70453031+infelipeq13@users.noreply.github.com>
2020-10-10 17:06:02 -0300

    Fix general styling

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -4,11 +4,11 @@ import css from "src/components/Layout/Layout.module.css";
 
 export const Layout = ({ children, description, imageName, title }) => {
 	return (
-		<div className={css.container}>
+		<main className={css.container}>
 			<Image className={css.image} name={imageName} />
 			<h1 className={css.title}>{title}</h1>
 			<p className={css.description}>{description}</p>
 			{children}
-		</div>
+		</main>
 	);
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,20 @@ import { Input } from "src/components/Input";
 import { Layout } from "src/components/Layout";
 
 const HomePage = () => {
+	const submit = (e) => {
+		e.preventDefault;
+	};
+
 	return (
 		<Layout
 			description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent porta maximus lacus."
 			imageName="paper-plane"
 			title="Acessar plataforma"
 		>
-			<Input label="Endereço de email" type="email" />
-			<Button isExpanded>Enviar acesso</Button>
+			<form className="w-full space-y-4" onSubmit={submit}>
+				<Input label="Endereço de email" type="email" />
+				<Button isExpanded>Enviar acesso</Button>
+			</form>
 		</Layout>
 	);
 };
