742a5bbc25b461779400b483a69369dda167b9df
infelipeq13 <70453031+infelipeq13@users.noreply.github.com>
2020-10-10 16:00:24 -0300

    Create Input component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,13 @@
 import { Button } from "src/components/Button";
+import { Input } from "src/components/Input";
 
 const HomePage = () => {
-	return <Button>Button</Button>;
+	return (
+		<>
+			<Button>Button</Button>
+			<Input label="Nome completo" errorMessage="Campo obrigatório." />
+		</>
+	);
 };
 
 export default HomePage;
