85eb131594bc78601ac35201e0c45393f5eb574a
infelipeq13 <70453031+infelipeq13@users.noreply.github.com>
2020-10-11 15:13:09 -0300

    Fix styling, create Divider component

--- src/components/DefinitionList/index.js
+++
@@ -1,5 +0,0 @@
-import css from "src/components/DefinitionList/DefinitionList.module.css";
-
-export const DefinitionList = ({ children }) => {
-	return <dl className={css.container}>{children}</dl>;
-};
