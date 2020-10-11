9a233259f5aae09cdb6cd9962666116df78061ef
infelipeq13 <70453031+infelipeq13@users.noreply.github.com>
2020-10-11 15:23:54 -0300

    Fix styling

--- src/components/DefinitionList.js
+++ src/components/DefinitionList.js
@@ -1,3 +1,3 @@
 export const DefinitionList = ({ children }) => {
-	return <dl className="p-4 space-y-2 bg-gray-100 rounded-lg text-x1">{children}</dl>;
+	return <dl className="p-4 space-y-2 bg-gray-100 rounded text-x1">{children}</dl>;
 };
